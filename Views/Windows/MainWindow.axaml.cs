57cde65a14c783e54583ce2a87c0c02099fd556d
SrS2225a <sawyer.r.stephenns@gmail.com>
2026-04-14 16:31:11 -0400

    forgot about the compiler warnings for axaml.cs

--- Views/Windows/MainWindow.axaml.cs
+++ Views/Windows/MainWindow.axaml.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
-using VSuiteLab.Models;
 using VSuiteLab.ViewModels;
 
-namespace VSuiteLab.Views;
+namespace VSuiteLab.Views.Windows;
 
 public partial class MainWindow : Window
 {
@@ -15,51 +10,13 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         
-        this.AddHandler(KeyDownEvent, OnGlobalKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+        AddHandler(KeyDownEvent, OnGlobalKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
     }
     
     private void OnGlobalKeyDown(object? sender, KeyEventArgs e)
     {
-        
-        if (DataContext is not MainWindowViewModel vm)
-            return;
-
-
-        if (!IsInputElementFocused())
-        {
-            switch (e.Key)
-            {
-                case Key.Oem2:
-                    if (SearchButton?.Flyout is Flyout flyout)
-                    {
-                        flyout.ShowAt(SearchButton);
-                    }
-                    break;
-
-                case Key.D1:
-                case Key.NumPad1:
-                    vm.SelectedTabIndex = 0;
-                    break;
-
-                case Key.D2:
-                case Key.NumPad2:
-                    vm.SelectedTabIndex = 1;
-                    break;
-
-                case Key.D3:
-                case Key.NumPad3:
-                    vm.SelectedTabIndex = 2;
-                    break;
-                default:
-                    return;
-            }
-        }
-    }
-
-    private bool IsInputElementFocused()
-    {
-        var topLevel = TopLevel.GetTopLevel(this);
-        var focused = topLevel?.FocusManager?.GetFocusedElement();
+        var topLevel = GetTopLevel(this);
+        var focused = topLevel?.FocusManager.GetFocusedElement();
 
 
         if (focused is TextBox ||
@@ -67,8 +24,39 @@ public partial class MainWindow : Window
             focused is NumericUpDown ||
             focused is DatePicker)
         {
-            return true;
+            return;
         }
-        return false;
+        
+        
+        if (DataContext is not MainWindowViewModel vm)
+            return;
+
+
+        switch (e.Key)
+        {
+            case Key.Oem2:
+                if (SearchButton?.Flyout is Flyout flyout)
+                {
+                    flyout.ShowAt(SearchButton);
+                }
+                break;
+
+            case Key.D1:
+            case Key.NumPad1:
+                vm.SelectedTabIndex = 0;
+                break;
+
+            case Key.D2:
+            case Key.NumPad2:
+                vm.SelectedTabIndex = 1;
+                break;
+
+            case Key.D3:
+            case Key.NumPad3:
+                vm.SelectedTabIndex = 2;
+                break;
+            default:
+                return;
+        }
     }
 }
