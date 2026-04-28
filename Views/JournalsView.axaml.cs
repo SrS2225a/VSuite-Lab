5ea973bb6b17ce61d1ab48128c6921be094306b4
SrS2225a <sawyer.r.stephens@gmail.com>
2026-04-28 18:42:38 -0400

    added save, update, delete commands for tasks, notes, and journals

--- Views/JournalsView.axaml.cs
+++ Views/JournalsView.axaml.cs
@@ -1,9 +1,11 @@
 using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using CommunityToolkit.Mvvm.ComponentModel;
 using VSuiteLab.Models;
+using VSuiteLab.ViewModels;
 
 namespace VSuiteLab.Views;
 
@@ -25,5 +27,42 @@ public partial class JournalsView : UserControl
     public JournalsView()
     {
         InitializeComponent();
+        
+        AddHandler(KeyDownEvent, OnGlobalKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+    }
+
+    private void OnGlobalKeyDown(object? sender, KeyEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var focused = topLevel?.FocusManager.GetFocusedElement();
+
+
+        if (focused is TextBox ||
+            focused is ComboBox ||
+            focused is NumericUpDown ||
+            focused is DatePicker)
+        {
+            return;
+        }
+
+
+        if (DataContext is JournalsViewModel vm)
+        {
+
+            switch (e.Key)
+            {
+                case Key.S:
+                    vm.SaveNewJournalCommand.Execute(null);
+                    break;
+                case Key.U:
+                    vm.SaveJournalCommandCommand.Execute(null);
+                    break;
+                case Key.D:
+                    vm.DeleteJournalCommandCommand.Execute(null);
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
--- Views/TasksView.axaml.cs
+++ Views/TasksView.axaml.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using CommunityToolkit.Mvvm.ComponentModel;
 using VSuiteLab.Models;
@@ -27,5 +28,42 @@ public partial class TasksView : UserControl
     public TasksView()
     {
         InitializeComponent();
+        
+        AddHandler(KeyDownEvent, OnGlobalKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+    }
+    
+    private void OnGlobalKeyDown(object? sender, KeyEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var focused = topLevel?.FocusManager.GetFocusedElement();
+
+
+        if (focused is TextBox ||
+            focused is ComboBox ||
+            focused is NumericUpDown ||
+            focused is DatePicker)
+        {
+            return;
+        }
+
+
+        if (DataContext is TasksViewModel vm)
+        {
+
+            switch (e.Key)
+            {
+                case Key.S:
+                    vm.SaveNewNoteCommand.Execute(null);
+                    break;
+                case Key.U:
+                    vm.SaveNoteCommand.Execute(null);
+                    break;
+                case Key.D:
+                    vm.DeleteNoteCommand.Execute(null);
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
