545eea612043442f11432a6175f7541b8ae1f770
SrS2225a <sawyer.r.stephenns@gmail.com>
2026-04-28 18:42:38 -0400

    added save, update, delete commands for tasks, notes, and journals

--- Views/NotesView.axaml.cs
+++ Views/NotesView.axaml.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using CommunityToolkit.Mvvm.ComponentModel;
 using VSuiteLab.Models;
@@ -26,5 +27,42 @@ public partial class NotesView : UserControl
     public NotesView()
     {
         InitializeComponent();
+        
+        AddHandler(KeyDownEvent, OnGlobalKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+    }
+    
+    private void OnGlobalKeyDown(object? sender, KeyEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var focused = topLevel?.FocusManager.GetFocusedElement();
+
+
+        if (focused is TextBox ||
+            focused is ComboBox ||
+            focused is NumericUpDown ||
+            focused is DatePicker)
+        {
+            return;
+        }
+
+
+        if (DataContext is NotesViewModel vm)
+        {
+
+            switch (e.Key)
+            {
+                case Key.S:
+                    vm.SaveNewJournalCommand.Execute(null);
+                    break;
+                case Key.U:
+                    vm.SaveJournalCommandCommand.Execute(null);
+                    break;
+                case Key.D:
+                    vm.DeleteJournalCommandCommand.Execute(null);
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
