8b99218741dea940e1b15ddcfcfeecc8015c6b91
WikiBotJak <contact@fenriris.net>
2026-04-30 22:59:42 -0400

    if you set an existing alarm date to the future, it will now reenable the trigger for it

--- Models/CalDavAlarm.cs
+++ Models/CalDavAlarm.cs
@@ -16,22 +16,39 @@ public class CalDavAlarm
     
     public DateTimeOffset? SelectedDate { get; set; } = null;
     
+    
     [NotMapped]
     public DateTimeOffset? AlarmDateOnly
     {
         get => TimeConverter.GetDateOnly(SelectedDate);
-        set => SelectedDate = TimeConverter.SetDateOnly(SelectedDate, value);
+        set
+        {
+            SelectedDate = TimeConverter.SetDateOnly(SelectedDate, value);
+            ResetIfMovedForward();
+        }
     }
 
     [NotMapped]
     public TimeSpan? AlarmTimeOnly
     {
         get => TimeConverter.GetTimeOnly(SelectedDate);
-        set => SelectedDate = TimeConverter.SetTimeOnly(SelectedDate, value);
+        set
+        {
+            SelectedDate = TimeConverter.SetTimeOnly(SelectedDate, value);
+            ResetIfMovedForward();
+        }
     }
 
     public int? Repeat { get; set; }
     public bool HasRan { get; set; } = false;
     public Guid CalDavItemId { get; set; }
     public CalDavItem CalDavItem { get; set; }
+    
+    private void ResetIfMovedForward()
+    {
+        if (HasRan && SelectedDate > DateTimeOffset.UtcNow)
+        {
+            HasRan = false;
+        }
+    }
 }
